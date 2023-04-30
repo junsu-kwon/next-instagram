c7838fa1c7f99dc6ba65d70216ad109ad75f04ee
junsu-kwon <kjs940322@gmail.com>
2023-04-30 23:36:33 +0900

    230429 공부

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import { Open_Sans } from 'next/font/google';
 import Navbar from '@/components/Navbar';
 import AuthContext from '../context/AuthContext';
+import { SWRConfig } from 'swr';
 
 const openSans = Open_Sans({ subsets: ['latin'] });
 
@@ -21,12 +22,21 @@ export default function RootLayout({
   return (
     <html lang="en" className={openSans.className}>
       <body className="w-full max-w-screen-xl overflow-auto mx-auto">
-        <AuthContext>
-          <header className="sticky top-0 bg-white z-10 border-b">
-            <Navbar />
-          </header>
-          <main>{children}</main>
-        </AuthContext>
+        {/* 글로벌 fetcher 설정 */}
+        <SWRConfig
+          value={{
+            refreshInterval: 3000,
+            fetcher: (resource, init) =>
+              fetch(resource, init).then((res) => res.json()),
+          }}
+        >
+          <AuthContext>
+            <header className="sticky top-0 bg-white z-10 border-b">
+              <Navbar />
+            </header>
+            <main>{children}</main>
+          </AuthContext>
+        </SWRConfig>
       </body>
     </html>
   );
--- src/components/FollowingBar.tsx
+++ src/components/FollowingBar.tsx
@@ -1,3 +1,46 @@
+'use client';
+
+import { Suspense } from 'react';
+import { PropagateLoader } from 'react-spinners';
+import Avatar from './Avatar';
+import Link from 'next/link';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import useSWR from 'swr';
+
+const responsive = {
+  superLargeDesktop: {
+    breakpoint: { max: 4000, min: 3000 },
+    items: 5,
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+};
+
 export default function FollowingBar() {
-  return <h1>FollowingBar</h1>;
+  const { data, error, isLoading, isValidating } = useSWR('/api/user/me');
+
+  return (
+    <Suspense fallback={<PropagateLoader color="#36d7b7" />}>
+      <Carousel responsive={responsive}>
+        {/* {allUser.map(({ username, image }) => {
+          return (
+            <Link href={`/user/${username}`} key={username}>
+              <Avatar image={image} size="small" highlight />
+            </Link>
+          );
+        })} */}
+      </Carousel>
+    </Suspense>
+  );
 }
